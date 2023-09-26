e77bc64be9a09cbab9a98df0ed85606a97153b1d
Ftthreign <theownerkill432@gmail.com>
2023-09-26 13:13:20 +0700

    Add Comment

--- Chap_1/main.py
+++ Chap_1/main.py
@@ -1,28 +1,34 @@
 import cv2
 import numpy as np
 
-
+# Read Image
 image = cv2.imread('hyerin.jpeg')
 
-get_r = image[:,:,0]
+# Get RGB Value
+get_b = image[:,:,0]
 get_g = image[:,:,1]
-get_b = image[:,:,2]
+get_r = image[:,:,2]
 
+# Get column and row from image
 get_row = len(image)
 get_col = len(image[0])
 
+# Get zoom to 0.5x from original size
 scaling_factor = 0.5  
-
-
 resized_image = cv2.resize(image, None, fx=scaling_factor, fy=scaling_factor)
 
+# Convert image to Grayscale
 img_grey = np.zeros((get_row, get_col), dtype=np.uint8)
+
+# loop to get Matrixes point
 for row in range(get_row):
     for col in range(get_col):
-        img_grey[row, col] = round(0.299 * get_b[row, col] + 0.587 * get_g[row, col] + 0.114 * get_r[row, col])
+        img_grey[row, col] = round(0.299 * get_r[row, col] + 0.587 * get_g[row, col] + 0.114 * get_b[row, col])
 
 
+# Save all matrix info to txt file
 np.savetxt('get_matrixes.txt', img_grey, fmt='%d', delimiter='\t')
 
+# Show image window with 0.5x original size
 cv2.imshow('hyerin', resized_image)
 cv2.waitKey()
