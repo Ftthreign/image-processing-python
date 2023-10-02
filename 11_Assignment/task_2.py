77ead4a0d58dd5ff5ff3e0d266e5cc05ad8ea5a6
Ftthreign <theownerkill432@gmail.com>
2023-10-03 02:00:04 +0700

    Update Binary Image Output

--- 11_Assignment/task_2.py
+++ 11_Assignment/task_2.py
@@ -4,8 +4,12 @@ image = cv2.imread('haerin.jpg')
 
 
 grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+binary_image = cv2.threshold(grayscale_image, 127, 255, cv2.THRESH_BINARY)[1]
+
 
 cv2.imwrite('./out_file/grayscaleImage_TASK_2.jpg', grayscale_image)
+cv2.imwrite('./out_file/BinaryImage_TASK_2.jpg', binary_image)
 
 cv2.imshow("Grayscale Image", grayscale_image)
+cv2.imshow('Binary Image', binary_image)
 cv2.waitKey()
