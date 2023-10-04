4482e4027ab571324ae3626fc8c80750f447b472
Ftthreign <theownerkill432@gmail.com>
2023-10-04 12:34:52 +0700

    [#26] ADD Task_6 file to DONE

--- 11_Assignment/task_2.py
+++ 11_Assignment/task_2.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 
 image = cv2.imread('haerin.jpg')
 
@@ -6,6 +8,11 @@ image = cv2.imread('haerin.jpg')
 grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 binary_image = cv2.threshold(grayscale_image, 127, 255, cv2.THRESH_BINARY)[1]
 
+get_matrixes_binary = np.array(binary_image)
+get_matrixes_grayscale = np.array(grayscale_image)
+
+print(f'Grayscale image matrixes :\n{get_matrixes_grayscale}\n\n\n')
+print(f'Binary image matrixes : \n {get_matrixes_binary}')
 
 cv2.imwrite('./out_file/grayscaleImage_TASK_2.jpg', grayscale_image)
 cv2.imwrite('./out_file/BinaryImage_TASK_2.jpg', binary_image)
---
+++ 11_Assignment/task_6.py
@@ -0,0 +1,47 @@
+import cv2
+import matplotlib.pyplot as plt
+
+
+def equalize_image(image):
+    get_imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    r, g, b = cv2.split(get_imageRGB)
+
+    r_equalization = cv2.equalizeHist(r)
+    g_equalization = cv2.equalizeHist(g)
+    b_equalization = cv2.equalizeHist(b)
+
+    get_equalization = cv2.merge(
+        (r_equalization, g_equalization, b_equalization))
+
+    return get_equalization
+
+
+def get_histogram(image):
+    get_imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    r, g, b = cv2.split(get_imageRGB)
+
+    r_histogram = cv2.calcHist([r], [0], None, [256], [0, 256])
+    g_histogram = cv2.calcHist([g], [0], None, [256], [0, 256])
+    b_histogram = cv2.calcHist([b], [0], None, [256], [0, 256])
+
+    plt.figure(figsize=(12, 6))
+    plt.subplot(131)
+    plt.title('Red Channel Histogram')
+    plt.plot(r_histogram, color='red')
+
+    plt.subplot(132)
+    plt.title('Green Channel Histogram')
+    plt.plot(g_histogram, color='green')
+
+    plt.subplot(133)
+    plt.title('Blue Channel Histogram')
+    plt.plot(b_histogram, color='blue')
+
+    plt.tight_layout()
+    plt.show()
+
+
+image = cv2.imread('haerin.jpg')
+
+equalize = equalize_image(image)
+get_histogram(equalize)
