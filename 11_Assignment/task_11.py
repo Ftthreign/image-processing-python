2ad669104f1403724520a143306666776cda9e0a
Aku-Mars <marifinsyam73@gmail.com>
2023-10-30 16:22:23 +0700

    Update

--- 11_Assignment/task_11.py
+++ 11_Assignment/task_11.py
@@ -1,7 +1,17 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('haerin.jpg')
+# Baca gambar
+img = cv2.imread('haerin.jpg')
 
+# Buat kernel filter rata-rata
+kernel = np.ones((5, 5), np.float32) / 25
 
-cv2.waitKey()
+# Terapkan filter rata-rata menggunakan fungsi filter2D
+output = cv2.filter2D(img, -1, kernel)
+
+# Tampilkan gambar asli dan gambar hasil filtering
+cv2.imshow('Gambar Asli', img)
+cv2.imshow('Hasil Filtering', output)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- 11_Assignment/task_8.py
+++ 11_Assignment/task_8.py
@@ -1,6 +1,18 @@
 import cv2
+import numpy as np
 
-image = cv2.imread('haerin.jpg')
+# Baca dua citra
+image1 = cv2.imread('kacamata.jpg')
+image2 = cv2.imread('haerin.jpg')
 
+# Pastikan citra memiliki ukuran yang sama
+if image1.shape == image2.shape:
+    # Lakukan operasi aritmatika, contoh: pengurangan
+    result = cv2.subtract(image1, image2)
 
-cv2.waitKey()
+    # Tampilkan citra hasil
+    cv2.imshow('Hasil Operasi Aritmatika', result)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("Citra-citra harus memiliki ukuran yang sama")
