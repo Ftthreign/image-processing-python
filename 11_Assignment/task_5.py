f927fb571680342895dce37dda3b9d8b370c336b
Ftthreign <theownerkill432@gmail.com>
2023-11-10 06:47:30 +0700

    remove break line

--- 11_Assignment/task_5.py
+++ 11_Assignment/task_5.py
@@ -3,13 +3,11 @@ import matplotlib.pyplot as plt
 
 image = cv2.imread('haerin.jpg')
 
-
 color = ('b', 'g', 'r')
 
 for i, column in enumerate(color):
     getHist = cv2.calcHist([image], [i], None, [256], [0, 256])
     plt.plot(getHist, color=column)
     plt.xlim([0, 256])
-
 
 plt.show()
