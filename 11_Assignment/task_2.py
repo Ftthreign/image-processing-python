26e8b8178d97c4771b81d0e84535b3fe5aa7a5c8
Ftthreign <theownerkill432@gmail.com>
2023-11-10 06:47:15 +0700

    add flattened array and give range to 100

--- 11_Assignment/task_2.py
+++ 11_Assignment/task_2.py
@@ -11,12 +11,13 @@ binary_image = cv2.threshold(grayscale_image, 127, 255, cv2.THRESH_BINARY)[1]
 get_matrixes_binary = np.array(binary_image)
 get_matrixes_grayscale = np.array(grayscale_image)
 
-print(f'Grayscale image matrixes :\n{get_matrixes_grayscale}\n\n\n')
-print(f'Binary image matrixes : \n {get_matrixes_binary}')
+print(
+    f'Grayscale image matrixes :\n{get_matrixes_grayscale.flatten()[:100]}\n\n\n')
+print(f'Binary image matrixes : \n {get_matrixes_binary.flatten()[:100]}')
 
 cv2.imwrite('./out_file/grayscaleImage_TASK_2.jpg', grayscale_image)
 cv2.imwrite('./out_file/BinaryImage_TASK_2.jpg', binary_image)
 
 cv2.imshow("Grayscale Image", grayscale_image)
 cv2.imshow('Binary Image', binary_image)
-cv2.waitKey()
+cv2.waitKey(0)
