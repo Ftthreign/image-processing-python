6b80f4b4f5f74ed88c013722e185f977f1e0078d
Ftthreign <theownerkill432@gmail.com>
2023-12-07 23:15:26 +0700

    fix: fix metode

--- ProjectAkhir/main.py
+++ ProjectAkhir/main.py
@@ -22,13 +22,10 @@
 #     x, y, w, h = cv2.boundingRect(c)
 #     if w > 30 and h > 30:
 #         cv2.drawContours(black_image, [c], -1, (255, 255, 255), -1)
-# # cv2.imshow("Original Image", resized_image)
-# # cv2.imshow("Edge Detection", edges)
-# # cv2.imshow("Dilation", dilated)
+
 # cv2.imshow("Contours Image", black_image)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
-
 
 import cv2
 import numpy as np
@@ -47,17 +44,17 @@ sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)
 sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)
 edges_sobel = np.sqrt(sobelx**2 + sobely**2)
 
-# Deteksi tepi menggunakan metode Prewitt
-kernelx = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
-kernely = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
-prewittx = cv2.filter2D(gray, -1, kernelx)
-prewitty = cv2.filter2D(gray, -1, kernely)
-edges_prewitt = np.sqrt(prewittx**2 + prewitty**2)
+adaptive_thres = cv2.adaptiveThreshold(
+    gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
-# Tampilkan hasil
-cv2.imshow("Original Image", resized_image)
-cv2.imshow("Canny Edge Detection", edges_canny)
-cv2.imshow("Sobel Edge Detection", edges_sobel.astype(np.uint8))
-cv2.imshow("Prewitt Edge Detection", edges_prewitt.astype(np.uint8))
+cv2.imwrite('./Hasil/Sampel_1_Grayscale.jpg', gray)
+cv2.imwrite('./Hasil/Sampel_1_Canny.jpg', edges_canny)
+cv2.imwrite('./Hasil/Sampel_1_Sobel.jpg', edges_sobel)
+
+# cv2.imshow("Grayscale Image", gray)
+cv2.imshow('Threshold', adaptive_thres)
+# cv2.imshow("Canny Edge Detection", edges_canny)
+# cv2.imshow("Sobel Edge Detection", edges_sobel.astype(np.uint8))
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
