4a1d4e7ad268349b4417e142e98ffd19e5347aa1
Ftthreign <theownerkill432@gmail.com>
2023-12-09 22:54:03 +0700

    update: add humanable name method array

--- ProjectAkhir/template_matching.py
+++ ProjectAkhir/template_matching.py
@@ -10,6 +10,9 @@ template = cv2.imread('./Gambar/Mobil_8.jpg', cv2.IMREAD_GRAYSCALE)
 assert template is not None, "file could not be read, check with os.path.exists()"
 w, h = template.shape[::-1]
 
+
+humanable_name_methods = ['Korelasi Koefisien', 'Krelasi Koefisien Normalisasi', 'Korelasi Silang',
+                          'Korelasi Silang Normalisasi', 'Perbedaan Kuadrat', 'Perbedaan Kuadrat yang dinormalisasi']
 methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
 
@@ -36,9 +39,12 @@ for meth in methods:
     bottom_right = (top_left[0] + w, top_left[1] + h)
     cv2.rectangle(img, top_left, bottom_right, 255, 2)
 
+for human_meth in humanable_name_methods:
     plt.subplot(121), plt.imshow(res, cmap='gray')
-    plt.title(f'Matching Result {meth}'), plt.xticks([]), plt.yticks([])
+    plt.title(f'Matching Result {human_meth}'), plt.xticks(
+        []), plt.yticks([])
     plt.subplot(122), plt.imshow(img, cmap='gray')
-    plt.title(f'Detected Point {meth}'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(meth)
+    plt.title(f'Detected Point {human_meth}'), plt.xticks(
+        []), plt.yticks([])
+    plt.suptitle(human_meth)
     plt.show()
